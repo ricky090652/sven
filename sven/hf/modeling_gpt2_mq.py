a05d06872fd03632f733ce909507ffa86f33f6fa
Ricky <ricky090652@gmail.com>
2026-02-06 00:05:19 +0800

    delete package SequenceSummary

--- sven/hf/modeling_gpt2_mq.py
+++ sven/hf/modeling_gpt2_mq.py
@@ -19,7 +19,7 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutputWithPast,
     TokenClassifierOutput,
 )
-from transformers.modeling_utils import PreTrainedModel, SequenceSummary
+from transformers.modeling_utils import PreTrainedModel
 from transformers.pytorch_utils import Conv1D, find_pruneable_heads_and_indices, prune_conv1d_layer
 
 from transformers.utils import (
